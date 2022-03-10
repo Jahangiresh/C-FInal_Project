f18e459e4dd3812093816ff4fa391d845fd63f4d
jahangir <98384389+Jahangiresh@users.noreply.github.com>
2022-03-10 19:38:34 +0400

    Add files via upload

---
+++ service/ServiceCode.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFinal
+{
+    class ServiceCode : IServiceCode
+    {
+        private List<Group> _groups = new List<Group>();
+        public List<Group> Groups => _groups;
+        private static List<Student> _students =  new List<Student>();
+        public static List<Student> Students = _students;
+        public bool CheckGroup(string no)
+        {
+            for (int i = 0; i < no.Length; i++)
+            {
+
+                if (no.Length == 4 && char.IsUpper(no[0]) && char.IsDigit(no[1]) &&char.IsDigit(no[2]) && char.IsDigit(no[3]))
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("GROUP NAME MUST CONTAIN 1 CAPITAL AND 3 DIGITS");
+                    break;
+                }
+                
+            }
+           
+            return false;
+        }
+        public bool CheckStudentName(string fullname)
+        {
+           
+            if (fullname.Contains(' '))
+            {
+                string[] subs = fullname.Split(' ');
+                string name = subs[0];
+                string surname = subs[1];
+                if (char.IsUpper(name[0]) && char.IsUpper(surname[0]))
+                {
+
+                    return true;
+                }
+                else
+                {
+
+                    Console.WriteLine("FIRST LETTERS MUST BE CAPITAL");
+                }
+            }
+            else
+            {
+                Console.WriteLine("THERE IS NO SPACE BETWEEN NAME AND SURNAME");
+                
+            }
+            return true;
+        }
+        public string CreateGroup(Category categories, string no, bool isOnline)
+        {
+            Group group = new Group(categories, no, isOnline);
+            if (!CheckGroup(no))
+            {
+               
+
+            }
+            if(isOnline == true)
+            {
+                group.Limit = 15;
+            }
+            else
+            {
+                group.Limit = 10;
+            }
+
+            Groups.Add(group);
+            return group.No;
+        }
+        public void ShowGroups()
+        {
+            if (_groups.Count == 0)
+            {
+                Console.Clear();
+                Console.Beep();
+                Console.Error.WriteLine("THERE IS NO GROUP YET");
+                return;
+            }
+            foreach (Group group in _groups)
+            {
+                Console.WriteLine(group);
+            }
+
+        }
+        public void EditGroupName(string no, string newNo)
+        {
+            Group existedGroup = FindGroup(no);
+
+            if (existedGroup == null)
+            {
+                Console.Clear();
+                Console.Beep();
+                Console.Error.WriteLine("THERE IS NO GROUP WITH THIS NAME");
+                return;
+            }
+
+            foreach (Group group in _groups)
+            {
+                if (group.No == newNo)
+                {
+                    Console.WriteLine($"{newNo} GROUP IS ALREADY EXITS");
+                    return;
+                }
+            }
+            existedGroup.No = newNo;
+            Console.WriteLine($"GROUP {no} SUCCESFULY CHANGED TO {newNo}");
+        }
+        public Group FindGroup(string no)
+        {
+            foreach (Group group in Groups)
+            {
+                if (group.No == no)
+                {
+                    return group;
+          
+                }
+            }
+            return null;
+            
+        }
+        public void GetGroupStudents(string no)
+        {
+            Group group = FindGroup(no);
+            if (group == null)
+            {
+                Console.Clear();
+                Console.Beep();
+                Console.Error.WriteLine("PLEASE CHOOSE VALID GROUP NUM");
+               
+                return;
+            }
+            if (Students.Count!=0)
+            {
+                foreach (Student student in group.Students)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            else
+            {
+
+                Console.Clear();
+                Console.Beep();
+                Console.Error.WriteLine($"THERE IS NOT ANY STUDENT IN GROUP: {group}");
+            
+                
+            }
+            
+        }
+        public void GetAllStudents()
+        {
+            if (Students.Count!=0)
+            {
+                foreach (Student student in Students)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.Beep();
+                Console.Error.WriteLine("THERE IS NO STUDENTS IN THIS ACADEMY" ); 
+
+
+            }
+           
+        }
+        public bool CheckGaranty(bool hasgaranty)
+        {
+            if (hasgaranty)
+            {
+                return true;
+            }
+            return false;
+        }
+       
+        public void CreateStudent(string fullname, string groupno,bool hasgaranty)
+        {
+
+            CheckStudentName(fullname);
+            
+           
+           
+            Student student = new Student(fullname, groupno,hasgaranty);
+            Group group = FindGroup(groupno);
+            group.Students.Add(student);
+            Students.Add(student);
+
+        }
+
+      
+
+     
+    }
+}
---
+++ service/ServiceMenu.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFinal.service
+{
+    static class ServiceMenu
+    {
+        public static ServiceCode serviceCode = new ServiceCode();
+        public static void CreateGroupMenu()
+        {
+
+
+            Console.WriteLine("TYPE GROUP NO");
+            string no = Console.ReadLine();
+            if (serviceCode.CheckGroup(no))
+            {
+                Console.WriteLine("1.ONLINE GROUP\n2.OFFLINE GROUP");
+                string strOnline = Console.ReadLine();
+                bool resultLimit = int.TryParse(strOnline, out int num);
+                bool isOnline = false;
+
+                if (resultLimit)
+                {
+                    switch (num)
+                    {
+
+                        case 1:
+                            isOnline = true;
+                            break;
+                        case 2:
+                            isOnline = false;
+                            break;
+                        default:
+                            Console.WriteLine("choose  valid num!  1 or  2 PLEASE ");
+                            return;
+                    }
+
+                }
+                //else
+                //{
+                //    Console.WriteLine("asad");
+                //    return;
+                //}
+                foreach (Category c in System.Enum.GetValues(typeof(Category)))
+                {
+                    Console.WriteLine($"{(int)c} {c}");
+                }
+                int category;
+                string catStr = Console.ReadLine();
+                bool resultCat = int.TryParse(catStr, out category);
+                if (resultCat)
+                {
+                    switch (category)
+                    {
+                        case (int)Category.Programming:
+
+                            string No = serviceCode.CreateGroup(Category.Programming, no, isOnline);
+                            Console.WriteLine($"{No} group succesfully created");
+                            break;
+                        case (int)Category.Design:
+
+                            No = serviceCode.CreateGroup(Category.Design, no, isOnline);
+                            Console.WriteLine($"{No} group succesfully created");
+                            break;
+                        case (int)Category.System_Administration:
+
+                            No = serviceCode.CreateGroup(Category.System_Administration, no, isOnline);
+                            Console.WriteLine($"{No} group succesfully created");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("please choose valid category");
+                }
+            }
+
+        }
+        public static void EditGroupMenu()
+        {
+            Console.WriteLine("TYPE GROUP NO");
+            string no = Console.ReadLine();
+            Console.WriteLine("TYPE NEW GROUP NO");
+            string newNo = Console.ReadLine();
+            if (newNo != no && serviceCode.CheckGroup(newNo))
+            {
+                serviceCode.EditGroupName(no, newNo);
+            }
+            else
+            {
+                Console.WriteLine("Yeni adi duz yazin");
+            }
+
+        }
+        public static void ShowGroupsMenu()
+        {
+            serviceCode.ShowGroups();
+        }
+        public static void GetGroupStudentsMenu()
+        {
+            Console.WriteLine("TYPE GROUP NO");
+            string no = Console.ReadLine();
+            serviceCode.GetGroupStudents(no);
+        }
+        public static void GetAllStudentsMenu()
+        {
+
+            serviceCode.GetAllStudents();
+        }
+        public static void CreateStudentMenu()
+        {
+
+            Console.WriteLine("TYPE NAME AND SURNAME");
+            string? fullname = Console.ReadLine();
+            Console.WriteLine("GROUP NO :");
+            string? qrupno = Console.ReadLine();
+            Group existedQrup = serviceCode.FindGroup(qrupno);
+
+            if (existedQrup == null)
+            {
+                Console.WriteLine("PLEASE CHOOSE EXISTED GROUP NO");
+                return;
+            }
+
+            if (existedQrup.Limit <= existedQrup.Students.Count)
+            {
+                Console.WriteLine("GROUP IS FULL");
+                return;
+            }
+
+            Console.WriteLine("1.with GARANTY\n2.without GARANTY");
+
+
+            int garant;
+            string? garantystr = Console.ReadLine();
+            bool resultCat = int.TryParse(garantystr, out garant);
+            if (garant==2||garant==1)
+            {
+                switch (garant)
+                {
+                    case 1:
+                        serviceCode.CheckGaranty(true);
+                        break;
+                    case 2:
+                        serviceCode.CheckGaranty(false);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("CHOOSE VALID NUM 1 OR 2"); return;
+            }
+            
+
+
+
+
+
+            Student student = new Student(fullname, qrupno, true);
+            serviceCode.CreateStudent(fullname, qrupno, true);
+
+
+            Console.WriteLine($"STUDENT {fullname} SUCCESFULLY CREATED");
+
+
+        }
+    }
+}
